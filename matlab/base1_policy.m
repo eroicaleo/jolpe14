9d9b791bd24314fac544a3f49637cf4d5544d219
Yang Ge <eroicaleo@gmail.com>
2014-09-13 23:34:24 -0700

    Added base1_policy.m

---
+++ matlab/base1_policy.m
@@ -0,0 +1,9 @@
+%%----------------------------------------------------------------------------------------------------
+% Here, we implement a baseline policy as following
+% We first use the default task mapping
+% then we start from the core with the highest temperature
+% and swap its task with a core with the lowest temperature and power less than it
+%%----------------------------------------------------------------------------------------------------
+function [T t_max power_optimum] = base1_policy(G_part, power, D)
+
+end
